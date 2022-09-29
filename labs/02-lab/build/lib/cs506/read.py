d47fbbfaa3317709f58b66427b5c6df5438e1f56
Brian <briantao@bu.edu>
2022-09-29 17:49:52 -0400

    unstaged commit

--- labs/02-lab/build/lib/cs506/read.py
+++ labs/02-lab/build/lib/cs506/read.py
@@ -1,7 +1,22 @@
+
+
+from openpyxl import NUMPY
+
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    
-    raise NotImplementedError()
+    reader = reader(csv_file_path)
+    matrix = None
+    first_row = True
+    for row_index, row in enumerate(reader):
+        if first_row:
+            size = len(row)
+            matrix = NUMPY.zeros((size, size), dtype=int)
+            first_row = False
+            # matrix[row_index] = [int(item) for item in row]
+        matrix[row_index] = row
+
+    return matrix
--- labs/02-lab/build/lib/cs506/sim.py
+++ labs/02-lab/build/lib/cs506/sim.py
@@ -5,12 +5,30 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(x)):
+        res += abs(x[i]-y[i])
+    return res
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    similar = 0
+    for i in range(len(x)):
+        for a in range(i):
+            if x[i] == y[a]:
+                similar += 1
+
+    total = len(x)*2-similar
+    index = 100*similar/total
+    return 100-index
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    similar = 0
+    for i in range(len(x)):
+        for a in range(i):
+            if x[i] == y[a]:
+                similar += 1
+
+    total = len(x)*2-similar
+    return 100*similar/total
 
 # Feel free to add more
